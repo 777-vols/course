d922a35c2be211068469995d6e567d50c89f479e
777-vols <63928270+777-vols@users.noreply.github.com>
2021-06-10 20:37:11 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+// ---------------Menu--------------------
+const btnMenu = document.querySelector('#menu');
+const el = document.getElementsByClassName('nav_link');
+const menu = document.querySelector('#myTopnav');
+const toggleMenu = function () {
+	menu.classList.toggle('responsive');
+}
+
+btnMenu.addEventListener('click', function (e) {
+	// e.stopPropagation();
+	toggleMenu();
+});
+
+for (var i = 0; i < el.length; i++) {
+	el[i].onclick = function (e) {
+		// e.stopPropagation();
+		toggleMenu();
+	}
+}
+
+document.addEventListener('click', function (e) {
+	const target = e.target;
+	const its_menu = target == menu || menu.contains(target);
+	const its_btnMenu = target == btnMenu;
+	const menu_is_active = menu.classList.contains('responsive');
+
+	if (!its_menu && !its_btnMenu && menu_is_active) {
+		toggleMenu();
+	}
+});
+
+// ---------Comments js------------------------------
+const HideButton = document.querySelector('#hide_but');
+const HideClass = document.querySelector('#H_class');
+
+const test = function () {
+	HideClass.classList.toggle('responsive');
+}
+
+HideButton.addEventListener('click', function (e) {
+	// e.stopPropagation();
+	test();
+});
